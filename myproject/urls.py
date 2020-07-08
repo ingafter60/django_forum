e41b34273448c8eeede7911efd1354388ae5d166
ing <inyoman_gurnitha@yahoo.com>
2020-07-09 02:34:52 +0700

    Create new accounts app and add sign up form

--- myproject/urls.py
+++ myproject/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
 
+from accounts import views as accounts_views
 from boards import views
 
 urlpatterns = [
 	path('', views.home, name='home'),
+	url(r'^signup/$', accounts_views.signup, name='signup'),
 	url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
 	url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
