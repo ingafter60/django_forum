f754047e8deab9674b03a3d92c574f35c5832b3e
ing <inyoman_gurnitha@yahoo.com>
2020-07-10 00:37:33 +0700

    List View - modified home view function from FBV to GCBV

--- boards/views.py
+++ boards/views.py
@@ -2,17 +2,20 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
-from django.views.generic import UpdateView
+from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
 
-from .forms import PostForm
-from .models import Topic
-def home(request):
-    boards = Board.objects.all()
-    return render(request, 'home.html', {'boards': boards})
+# def home(request):
+#     boards = Board.objects.all()
+#     return render(request, 'home.html', {'boards': boards})
+
+class BoardListView(ListView):
+    model = Board
+    context_object_name = 'boards'
+    template_name = 'home.html'    
 
 # def board_topics(request, pk):
 #     # board = Board.objects.get(pk=pk)
