d7fb5f759083a52bee24c80458911fc50521d0a3
ing <inyoman_gurnitha@yahoo.com>
2020-07-10 00:32:56 +0700

    Update View

--- boards/views.py
+++ boards/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.db.models import Count
+from django.views.generic import UpdateView
+from django.utils import timezone
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
@@ -54,7 +56,7 @@ def topic_posts(request, pk, topic_pk):
     topic.save()
     return render(request, 'topic_posts.html', {'topic': topic})
 
-    
+
 @login_required
 def reply_topic(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
@@ -69,3 +71,19 @@ def reply_topic(request, pk, topic_pk):
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic': topic, 'form': form})
+
+
+class PostUpdateView(UpdateView):
+    
+    model = Post
+    fields = ('message', )
+    template_name = 'edit_post.html'
+    pk_url_kwarg = 'post_pk'
+    context_object_name = 'post'
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.updated_by = self.request.user
+        post.updated_at = timezone.now()
+        post.save()
+        return redirect('topic_posts', pk=post.topic.board.pk, topic_pk=post.topic.pk)    
