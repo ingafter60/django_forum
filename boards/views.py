0fb63a496e9d3702763693c627a98cb93daf2ad7
ing <inyoman_gurnitha@yahoo.com>
2020-07-09 23:17:33 +0700

    QuerySets - Showing Posts and Topic counts

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 
 from .forms import NewTopicForm, PostForm
 from .models import Board, Topic, Post
@@ -11,10 +12,15 @@ def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
 
+# def board_topics(request, pk):
+#     # board = Board.objects.get(pk=pk)
+#     board = get_object_or_404(Board, pk=pk)
+#     return render(request, 'topics.html', {'board': board})
+
 def board_topics(request, pk):
-    # board = Board.objects.get(pk=pk)
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'topics.html', {'board': board})
+    topics = board.topics.order_by('-last_updated').annotate(replies=Count('posts') - 1)
+    return render(request, 'topics.html', {'board': board, 'topics': topics})
 
 @login_required
 def new_topic(request, pk):
