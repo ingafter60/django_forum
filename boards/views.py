6b6e6067d7fca86497baccb0e7a9313f74ed7952
ing <inyoman_gurnitha@yahoo.com>
2020-07-09 23:26:15 +0700

    Migrations - showing views count

--- boards/views.py
+++ boards/views.py
@@ -44,11 +44,17 @@ def new_topic(request, pk):
     return render(request, 'new_topic.html', {'board': board, 'form': form})
 
 
+# def topic_posts(request, pk, topic_pk):
+#     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
+#     return render(request, 'topic_posts.html', {'topic': topic})    
+
 def topic_posts(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
-    return render(request, 'topic_posts.html', {'topic': topic})    
+    topic.views += 1
+    topic.save()
+    return render(request, 'topic_posts.html', {'topic': topic})
 
-
+    
 @login_required
 def reply_topic(request, pk, topic_pk):
     topic = get_object_or_404(Topic, board__pk=pk, pk=topic_pk)
